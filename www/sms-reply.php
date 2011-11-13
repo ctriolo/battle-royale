a769ae175797839180b10b17fb613012f370b1fd
Rafael G. Romero <rgromero@tux.CS.Princeton.EDU>
2011-11-12 22:47:25 -0500

    Bugs fixed. Game name conversation

--- www/sms-reply.php
+++ www/sms-reply.php
@@ -1,11 +1,13 @@
 <?php
 
+    session_start();
+
     function send_reply($str) {
         header("content-type: text/xml");
         echo "<?xml version=\"1.0\" encoding=\"UTF-8\"" . chr(63) . ">\n";
-        echo "<Response>\n<Sms>\n";
+        echo "<Response>\n<Sms>";
         echo $str;
-        echo "\n</Response>\n</Sms>\n";
+        echo "\n</Sms>\n</Response>\n";
         exit();
     }
     
@@ -28,17 +30,21 @@
         
         $reply = "Welcome to Battle Royale! What is the name of your game? ";
         $reply .= "e.g. PrincetonStartupWeekend2011 or psw2011";
-        send_reply($reply);
-        
-        $_SESSION['awaiting_game_name'] = "true";
+                
+        $_SESSION['awaiting_game_name'] = 1;
+
+	send_reply($reply);
     }
     
-    if ($_SESSION['awaiting_game_name'] == "true") {
+    $agn = $_SESSION['awaiting_game_name'];
+    if ( strlen($agn) && $agn ) {
         $game_name = $body;
         $reply = "Your game has been created with name " . $game_name . ". ";
         $reply .= "Please tell players to text JOIN " . $game_name . " to enter game.";
         
-        $_SESSION['awaiting_game_name'] = "false";
+        $_SESSION['awaiting_game_name'] = 0;
+
+	send_reply($reply);
     }
     
     // JOIN GAME
